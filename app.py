2884dec9dc7f9cee49f65a02597c2a49cea36cac
Prasant Adhikari <pa1038@nyu.edu>
2022-11-10 11:19:34 -0500

    Create app.py
    The main file for python code. Will require having a sample table A in the database

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import psycopg2
+from configparser import ConfigParser
+
+@st.cache
+def get_config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser()
+    parser.read(filename)
+    return {k: v for k, v in parser.items(section)}
+
+db_info = get_config()
+conn = psycopg2.connect(**db_info)
+curr = conn.cursor()
+curr.execute("SELECT * from A;") # just dummy table I created
+data = curr.fetchall()
+conn.close()
+df = pd.DataFrame(data=data)
+st.dataframe(data)
