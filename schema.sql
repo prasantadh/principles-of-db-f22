baf47ab0078bd6e2a3811151d0c23bb4666c3118
Sonali Singh <83692145+sonaliketaki@users.noreply.github.com>
2022-12-14 00:19:52 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,12 +13,14 @@ create type DAYS as ENUM
 );
 
 /* entity set Terms */
+--populated
 create table Terms (
 	year integer primary key,
 	CHECK ( year > 2021 )
 );
 
 /* entity set Lessons */
+--populated
 create table Lessons (
 	day DAYS,
 	time tsrange,
@@ -28,7 +30,9 @@ create table Lessons (
 	exclude using gist (time with &&) /* have non-overlapping range only for lessons */
 );
 
+
 /* entity set Grades with relationship sets Lunch and Assembly */
+--populated
 create table Grades (
 	name char(32) primary key,
 	assembly_day DAYS
@@ -40,6 +44,7 @@ create table Grades (
 );
 
 
+--populated
 create table Lunch (
 	lunch_day DAYS not null,
 	lunch_time tsrange,
@@ -50,22 +55,26 @@ create table Lunch (
 	lesson per day */
 );
 
+
 /* entity set Students 
 with relationship set attend */
+--populated
 create table Students (
-	id integer primary key,
+	id varchar(128) primary key, --changed from int to varchar
 	name char(64),
 	attend char(32) references Grades(name) 
 		not null	/* total participation */
 );
 
 /* entity set Departments */
+--populated
 create table Departments (
 	name char(32) primary key
 );
 
 /* Teachers entity set
 with relationship set Works_in and Half_Day */
+--populated
 create table Teachers (
 	initials char(32) primary key,
 	half_day DAYS not null,
@@ -74,11 +83,13 @@ create table Teachers (
 );
 
 /* entity set Rooms */
+--populated
 create table Rooms (
-	name char(32) primary key
+	room_number integer primary key
 );
 
 /* entity set Subjects */
+--populated
 create table Subjects (
 	name char(32),
 	double_lesson boolean default false,
@@ -90,9 +101,10 @@ create table Subjects (
 );
 
 /* relationship set Schedule */
+--populated
 create table Schedule (
 	grade char(32) references Grades(name) not null,
-	room char(32) references Rooms(name) not null,
+	room integer references Rooms(room_number) not null,
 	teacher char(32) references Teachers(initials) not null,
 	term int references Terms(year) not null,
 	day DAYS not null,
